34ed3253d2c5997d56592d30d0db46ade3db1f07
anikmondol <anikmondol558363@gmail.com>
2024-07-12 23:29:12 +0600

    added this code in this github

---
+++ class-4/function.php
@@ -0,0 +1,39 @@
+<?php
+
+// indexed array
+// $munnas = [
+//     "name" => "fahim hossain",
+//     "age" => 28,
+//     "gender" => "male",
+//     "blood" => "A+",
+//     "contact" => "017*********"
+// ];
+
+
+// foreach($munnas as $munna){
+//     echo $munna . "<br>";
+// }
+
+
+
+
+// associative array
+// $munnas = [
+//     "fahim hossain",
+//     28,
+//     "male",
+//     "A+",
+//     "017*********"
+// ];
+
+
+// $length = count($munnas);
+
+// echo $length;
+
+// for ($i=0; $i < $length; $i++) { 
+//     echo "$munnas[$i] <br>";
+// }
+
+
+?>
