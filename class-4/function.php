d660b5b172a571c0f80d31d8f8c3abee69674b6b
anikmondol <anikmondol558363@gmail.com>
2024-07-12 23:56:56 +0600

    added this code in this github

--- class-4/function.php
+++ class-4/function.php
@@ -36,4 +36,64 @@
 // }
 
 
+
+// function xyz(){
+//     echo "amei valo ace <br>";
+// }
+
+// xyz();
+// xyz();
+// xyz();
+
+
+// ***function
+
+
+function sum($num1, $num2){
+    return $num1 + $num2 . "<br>";
+}
+
+
+echo sum(20, 20);
+echo sum(40, 20);
+echo sum(20, 70);
+
+
+
+function reuseable($name, $des){
+    echo "Name: $name";
+    echo "<br>";
+    echo "Description: $des";
+}
+
+
+reuseable("anik", "I love dhaka <br>");
+for ($i=1; $i <= 10; $i++) { 
+    echo "$i <br>";
+}
+reuseable("apu", "I love Bangaldesh <br>");
+if (true) {
+    echo "i am also happy <br>";
+}
+reuseable("apurbo", "I love india <br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
